17324d77e172795a2630f17e7a66a7b16585392e
dedyrosyadi <dedy.rosyadi@gmail.com>
2017-09-21 12:01:04 +0800

    tambah logo baru

--- src/concat/concat.js
+++ src/concat/concat.js
@@ -12452,5 +12452,29 @@ if (typeof jQuery === 'undefined') {
 }(jQuery);
 
 // console.log("gulp bootstrap starter");
+$('.page-scroll').on('click', function(e) {
+	var target = $(this).attr('href');
+
+	// $('body').scrollTop(target.offset().top);
+	$('body').animate({
+        scrollTop: $(target).offset().top
+    }, 800);
+
+	
+	// window.location = '/';
+	// e.preventDefault();
+});	
+
+
+
+
+function openNav() {
+    document.getElementById("menu-resv").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("menu-resv").style.height = "0%";
+}
+
 
 // console.log('@muslihzarth');
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
 // console.log("gulp bootstrap starter");
+$('.page-scroll').on('click', function(e) {
+	var target = $(this).attr('href');
+
+	// $('body').scrollTop(target.offset().top);
+	$('body').animate({
+        scrollTop: $(target).offset().top
+    }, 800);
+
+	
+	// window.location = '/';
+	// e.preventDefault();
+});	
+
+
+
+
+function openNav() {
+    document.getElementById("menu-resv").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("menu-resv").style.height = "0%";
+}
+
